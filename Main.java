270458e2d18e4b3a22567514eae0396a82fae8b2
Darya <Darya.yatchanka@gmail.com>
2019-05-21 19:17:24 -0400

    AliceBob thing

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
-
+        Scanner in = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = in.nextLine();
+        if(name.equalsIgnoreCase("alice")||name.equalsIgnoreCase("bob")){
+            System.out.println("Hello!");
+    }
     }
 }
+
